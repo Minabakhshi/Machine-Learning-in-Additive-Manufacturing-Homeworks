c9f14598a25d8b801a14b9a20aa928d3046699fd
Mina Bakhshi <minabakhshi1993@gmail.com>
2025-12-13 13:53:56 +0100

    Add files via upload

---
+++ Simple-Calculator.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 13 13:14:25 2025
+
+@author: Smart
+"""
+
+# Simple Calculator
+
+numb1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+  -  *  /): ")
+
+if operator == "+":
+    print(numb1 + numb2)
+
+elif operator == "-":
+    print(numb1 - numb2)
+
+elif operator == "*":
+    print(numb1 * numb2)
+
+elif operator == "/":
+    if numb2 == 0:
+        print("Error: division by zero is not allowed!")
+    else:
+        print(numb1 / numb2)
+
+else:
+    print("Invalid operator! Use only +  -  *  /")
