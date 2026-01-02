3357dc8b07d83485274ff4d960c87866b9577a1b
Mina Bakhshi <minabakhshi1993@gmail.com>
2026-01-02 18:17:06 +0100

    Add files via upload

---
+++ check_density_threshold.py
@@ -0,0 +1,13 @@
+#S4-Q6
+
+def check_density(density):
+    if density > 98:
+        return True
+    else:
+        return False
+
+
+# example usage
+print(check_density(99.2))   # True
+print(check_density(97.5))   # False
+
